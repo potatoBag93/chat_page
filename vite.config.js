e0834443c23b3d0877da15ebb920554ab9fc58c3
wyna3209 <skdnjsduq@naver.com>
2025-06-16 22:12:29 +0900

    init: 최초 커밋 및 배포 자동화 설정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // GitHub Pages 등 하위 경로 배포 시 필요
+  base: '/chat_page/',
   plugins: [react()],
 })
